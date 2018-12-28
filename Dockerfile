c4533dd0a7999504995245060acf08ee02fae375
Sami Kohvakka <sami.kohvakka@live.com>
2018-12-28 20:24:30 +0200

    Fixed some of the issues and made it run in Docker

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+﻿# Repository does not use node modules -> does not need node 
+FROM nginx:latest
+
+#WORKDIR /LUT/WWW-sovellukset\Harjoitustyö\
+
+#COPY Harjoitustyö.html./
+#COPY HT.js./
+
+# Repository does not contain package.json file -> no need to install node dependencies
+#RUN npm install
+
+COPY . /usr/share/nginx/html
+
+EXPOSE 80
+
+# Page does not rely on node backend -> start script is not defined
+#CMD [ "npm", "start" ]
--- HT.js
+++ HT.js
@@ -1,33 +1,38 @@
-"use strict"  
- var function = new Vue({ //vue funktion luonti//
-    el: '.js-instance', //elementti//
-    name: 'vue-instance', //vue-instanssin luonti. Tässä todennäköisesti jotain häikkää, koska ei toimi//
-	
-	data() {
-	return {
-		newItem: '', 	//listan yksi alkio
-		
-		items: [		// listan luonti //
-		
-		],
-	  };
-	},
+"use strict";
+var app = new Vue({
+  //vue funktion luonti//
 
-	mounted() {},
+  /* fixed binding  */
+  el: "#app", //elementti//
+  name: "vue-instance", //vue-instanssin luonti. Tässä todennäköisesti jotain häikkää, koska ei toimi//
 
-	methods: {
-	  removeItem(index) {
-		this.items.splice(index, 1); 
-	  },
-	
-	  add() {
-	    if (this.newItem === '') { // uuden alkion lisääminen listaan //
-		  return;
-		}
-		
-	    this.items.push(this.newItem); // alkion lisäys //
-	    this.newItem = ''; // uusi alkio //
-	  },
+  data: function () {
+    return {
+      newItem: "", //listan yksi alkio
+
+      items: [
+        // listan luonti //
+      ]
+    };
+  },
+
+  // removed unused lifecycle hook
+  //mounted() {},
+
+  methods: {
+    removeItem(index) {
+      this.items.splice(index, 1);
     },
-  });
-  
+
+    add: function () {
+      console.log("add called")
+      if (this.newItem === "") {
+        // uuden alkion lisääminen listaan //
+        return;
+      }
+
+      this.items.push(this.newItem); // alkion lisäys //
+      this.newItem = ""; // uusi alkio //
+    }
+  }
+});
